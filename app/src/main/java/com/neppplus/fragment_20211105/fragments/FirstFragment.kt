968e54974a8730ee3c186b26c5d4f66238647326
김준기 <gugi329@daum.net>
2021-11-05 12:35:29 +0900

    프래그먼트 클래스 추가   - Fragment() 상속   - xml만 커스텀. -> onCreateView 오버라이딩

---
+++ app/src/main/java/com/neppplus/fragment_20211105/fragments/FirstFragment.kt
@@ -0,0 +1,20 @@
+package com.neppplus.fragment_20211105.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.fragment_20211105.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate( R.layout.fragment_first, container, false )
+    }
+
+}
