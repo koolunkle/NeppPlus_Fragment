6586a82801035fa425d0bd61149c48271c622f0e
김준기 <gugi329@daum.net>
2021-11-05 12:37:40 +0900

    프래그먼트 동작 코드  - 화면 전달 : requireContext() 활용

--- app/src/main/java/com/neppplus/fragment_20211105/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20211105/fragments/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.neppplus.fragment_20211105.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20211105.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -15,6 +18,19 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate( R.layout.fragment_first, container, false )
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLog.setOnClickListener {
+            Log.d("첫프래그먼트", "테스트로그")
+        }
+
+        btnToast.setOnClickListener {
+            Toast.makeText(requireContext(), "프래그먼트 토스트", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 }
